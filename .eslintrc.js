de4b1575128f06e71d49ceac0f9b53253da9a9bb
laurent lahmy <lahmy.laurent@gmail.com>
2022-06-17 15:53:10 +0800

    remove warnings for tailwindcss/classnames-order

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
       },
     ],
     "tailwindcss/no-custom-classname": "off",
+    "tailwindcss/classnames-order": "off",
     "import/extensions": [
       "error",
       "ignorePackages",
